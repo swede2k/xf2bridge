5a451ce508b8f35b3f563b733290a76aa9562b59
culv3r <danny.r.larsen@gmail.com>
2018-07-15 19:42:24 -0500

    Further Refactoring

--- src/XF2Bridge/XF2BridgeServiceProvider.php
+++ src/XF2Bridge/XF2BridgeServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class XenforoBridgeServiceProvider extends ServiceProvider {
+class XF2BridgeServiceProvider extends ServiceProvider {
 
 	/**
 	 * Indicates if loading of the provider is deferred.
